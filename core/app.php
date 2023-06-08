595b18ce3550a06593b5ab614c45db38f8d8c7a5
kashif <admin@kashifali.me>
2023-06-08 20:19:15 +0500

    test

--- core/app.php
+++ core/app.php
@@ -1,5 +1,7 @@
 <?php
 header("Content-Type: application/json");
+echo "Test";
+exit();
 
 $hostname = "localhost";
 $user = "u953547654_app";
@@ -7,8 +9,6 @@ $pwd = "App@123455";
 $db = "u953547654_app";
 $con = mysqli_connect($hostname, $user, $pwd, $db);
 
-echo "Test";
-exit();
 $response = array(
     'status' => false,
     'message' => '',
