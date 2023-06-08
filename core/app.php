060beeeb41ee03a501c0ac31c560d41dc22160e2
kashif <admin@kashifali.me>
2023-06-08 20:18:49 +0500

    t

--- core/app.php
+++ core/app.php
@@ -1,3 +1,20 @@
 <?php
+header("Content-Type: application/json");
+
+$hostname = "localhost";
+$user = "u953547654_app";
+$pwd = "App@123455";
+$db = "u953547654_app";
+$con = mysqli_connect($hostname, $user, $pwd, $db);
 
 echo "Test";
+exit();
+$response = array(
+    'status' => false,
+    'message' => '',
+    'data' => array()
+);
+if ($con->connect_errno) {
+    echo "Failed to connect to MySQL: " . $con->connect_error;
+    exit();
+}
