8a3649498648a798ce0a19ec2f38d89fbe052737
kashif <admin@kashifali.me>
2023-06-11 13:22:12 +0500

    Mail

--- core/app.php
+++ core/app.php
@@ -1,4 +1,9 @@
 <?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
 header("Content-Type: application/json");
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
@@ -33,5 +38,36 @@ function generateOPT($con, $email){
     $stmt = mysqli_prepare($con, $query);
     mysqli_stmt_bind_param($stmt, 'is', $otp, $email);
     mysqli_stmt_execute($stmt);
+
+    sendEmailOTP($email, $otp);
     return $otp;
+}
+
+function sendEmailOTP($email, $otp) {
+    // Create a new PHPMailer instance
+    $mail = new PHPMailer();
+
+    // SMTP Configuration (Replace with your own SMTP settings)
+    $mail->isSMTP();
+    $mail->Host = 'smtp.hostinger.com';
+    $mail->Port = 587;
+    $mail->SMTPAuth = true;
+    $mail->Username = 'gym@kashifali.me';
+    $mail->Password = 'Gym@1234';
+    $mail->SMTPSecure = 'tls';
+
+    // Set the From and To addresses
+    $mail->setFrom('gym@kashifali.me', 'Gym APP');
+    $mail->addAddress($email);
+
+    // Set the subject and body of the email
+    $mail->Subject = 'OTP Verification';
+    $mail->Body = 'Your OTP is: ' . $otp;
+
+    // Send the email
+    if ($mail->send()) {
+        return true;
+    } else {
+        return false;
+    }
 }
