e2122929df30973a06dd3ba685df4d0231408f92
kashif <admin@kashifali.me>
2023-07-21 10:51:43 +0500

    Added is_rated parameter in user/gyms

--- core/app.php
+++ core/app.php
@@ -7,15 +7,15 @@ const APIKEY = 'AIzaSyCJBr0IOmZ5jL8vtfNK30gVzpd_fGc1xm4';
 
 
 
+$hostname = "localhost";
+$user = "root";
+$pwd = "";
+$db = "gymApp";
 
 $user = "u953547654_app";
 $pwd = "App@12345";
 $db = "u953547654_app";
 
-$hostname = "localhost";
-$user = "root";
-$pwd = "";
-$db = "gymApp";
 
 
 
